933b4c821f4f223f3c38a4d8a60c80842984a820
Marco Jovanovic <marco_jovanovic_email@yahoo.com>
2024-09-12 10:44:54 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SLickCarousel from '../SlickCarousel';
+
+function App() {
+  return (
+    <div>
+      <SLickCarousel />
+    </div>
+  );
+}
+
+export default App;
